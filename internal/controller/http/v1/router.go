5853ed7e326da684261a4a49e8a1af52d76966a1
Roman Shelekhov <shelekhoff@gmail.com>
2025-02-12 09:58:49 +0400

    implement router

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -1,1 +1,44 @@
 package v1
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/rshelekhov/avito-tech-internship/internal/lib/middleware/jwt"
+)
+
+type Router struct {
+	log          *slog.Logger
+	jwtMgr       jwt.Manager
+	authHandler  AuthHandler
+	coinsHandler CoinsHandler
+}
+
+type (
+	AuthHandler interface {
+		Auth() http.HandlerFunc
+	}
+
+	CoinsHandler interface {
+		GetInfo() http.HandlerFunc
+		SendCoin() http.HandlerFunc
+		BuyCoin() http.HandlerFunc
+	}
+)
+
+func NewRouter(
+	log *slog.Logger,
+	jwtMgr jwt.Manager,
+	authHandler AuthHandler,
+	coinsHandler CoinsHandler,
+) *chi.Mux {
+	ar := &Router{
+		log:          log,
+		jwtMgr:       jwtMgr,
+		authHandler:  authHandler,
+		coinsHandler: coinsHandler,
+	}
+
+	return ar.initRoutes()
+}
