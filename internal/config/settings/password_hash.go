54504a4ba80eed41366c407a2751649a9648b763
Roman Shelekhov <shelekhoff@gmail.com>
2025-02-12 09:57:50 +0400

    add app config

--- internal/config/settings/password_hash.go
+++ internal/config/settings/password_hash.go
@@ -1,1 +1,15 @@
 package settings
+
+import "github.com/rshelekhov/avito-tech-internship/internal/domain/service/token"
+
+type PasswordHash struct {
+	Pepper     string `mapstructure:"PASSWORD_HASH_PEPPER"`
+	BcryptCost int    `mapstructure:"PASSWORD_HASH_BCRYPT_COST" envDefault:"10"`
+}
+
+func ToPasswordHashConfig(params PasswordHash) token.PasswordHash {
+	return token.PasswordHash{
+		Pepper:     params.Pepper,
+		BcryptCost: params.BcryptCost,
+	}
+}
