eea451962cfeaca9cf2e6f51c67674c8dab823b8
Roman Shelekhov <shelekhoff@gmail.com>
2025-02-12 10:02:33 +0400

    add healthcheck

--- internal/controller/http/v1/health.go
+++ internal/controller/http/v1/health.go
@@ -1,1 +1,12 @@
 package v1
+
+import "net/http"
+
+func HealthCheck() http.HandlerFunc {
+	return func(w http.ResponseWriter, _ *http.Request) {
+		_, err := w.Write([]byte("OK"))
+		if err != nil {
+			return
+		}
+	}
+}
