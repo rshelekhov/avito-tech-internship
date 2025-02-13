718a7e784c37f391093b4032a8619d04091d8df7
Roman Shelekhov <shelekhoff@gmail.com>
2025-02-13 23:59:16 +0400

    add postgres storage

--- internal/infrastructure/storage/storage.go
+++ internal/infrastructure/storage/storage.go
@@ -1,1 +1,43 @@
 package storage
+
+import (
+	"fmt"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	pgStorage "github.com/rshelekhov/avito-tech-internship/pkg/storage/postgres"
+)
+
+type DBConnection struct {
+	Postgres *Postgres
+}
+
+type Postgres struct {
+	Pool *pgxpool.Pool
+}
+
+func NewDBConnection(cfg Config) (*DBConnection, error) {
+	return newPostgresStorage(cfg)
+}
+
+func newPostgresStorage(cfg Config) (*DBConnection, error) {
+	const method = "storage.newPostgresStorage"
+
+	pool, err := pgStorage.New(cfg.Postgres)
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to create new postgres storage: %w", method, err)
+	}
+
+	return &DBConnection{
+		Postgres: &Postgres{
+			Pool: pool,
+		},
+	}, nil
+}
+
+type Config struct {
+	Postgres *pgStorage.Config
+}
+
+func (d *DBConnection) Close() {
+	pgStorage.Close(d.Postgres.Pool)
+}
